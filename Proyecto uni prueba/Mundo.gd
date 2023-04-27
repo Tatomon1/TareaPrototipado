9102fdf6fe356788984ebfad94d76bc8a1719dc3
Franco Angelini (Tato) <44816534+Tatomon1@users.noreply.github.com>
2023-04-27 14:13:35 -0300

    Add files via upload

---
+++ Proyecto uni prueba/Mundo.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+#	for i in 3:
+#		print("Hola " + str(i))
+	pass
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Proyecto uni prueba/Personaje.gd
@@ -0,0 +1,71 @@
+extends CharacterBody2D
+
+
+const SPEED = 350.0
+const JUMP_VELOCITY = -400.0
+const FRICTION = 1.2
+const gravity = 20
+var friccion: int = 0
+var Doble_salto: int = 0 
+
+
+func _physics_process(delta):
+	MovimientoPj(delta)     # Mueve al pj.
+	Doblesalto()
+	Planear()
+	# Agrego la gravedad
+	if not is_on_floor():
+		velocity.y += gravity
+
+
+#	if Input.is_action_just_released("ui_left"):
+#		velocity.x -= SPEED - 500
+##		if velocity.x <= 0:
+##			velocity.x = 0
+#
+#
+
+	if Input.is_action_just_released("MoverDerecha"):
+		
+		pass
+	move_and_slide()
+
+	pass
+
+
+
+
+
+
+
+#Funciones:
+func Planear():
+	if Input.is_action_pressed("Planear"):
+		if velocity.y > 0 and !is_on_floor():
+			velocity.y = 30 
+
+func Doblesalto():
+		if Input.is_action_just_pressed("ui_up") and Doble_salto < 2:
+			velocity.y = JUMP_VELOCITY
+			Doble_salto += 1
+
+		if Input.is_action_just_pressed("ui_up") and not is_on_floor() and Doble_salto < 2:
+			velocity.y = JUMP_VELOCITY
+			Doble_salto += 2
+
+		if Doble_salto >= 2 and is_on_floor():
+			Doble_salto = 0
+
+
+func MovimientoPj(delta):
+	var direction = Input.get_axis("ui_left", "MoverDerecha")
+	if direction:
+		velocity.x = direction * SPEED
+		friccion = 0
+	else:
+			velocity.x = move_toward(velocity.x,0,SPEED)
+#			friccion += FRICTION * delta
+#			friccion = clamp(friccion,0,1)
+
+
+
